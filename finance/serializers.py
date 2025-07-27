88e112c64b5ec9d4af47bd7234a0130fbd1883f0
enoskyeza <enoskyeza@gmail.com>
2025-07-27 22:26:34 +0300

    create fiance serializers

---
+++ finance/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from finance.models import (
+    Party, Account, Invoice, Payment, Transaction,
+    Goal, GoalMilestone, Requisition
+)
+
+
+class PartySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Party
+        fields = '__all__'
+
+
+class AccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = '__all__'
+
+
+class InvoiceSerializer(serializers.ModelSerializer):
+    balance = serializers.DecimalField(source='balance', max_digits=12, decimal_places=2, read_only=True)
+
+    class Meta:
+        model = Invoice
+        fields = '__all__'
+
+
+class GoalMilestoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = GoalMilestone
+        fields = '__all__'
+
+
+class GoalSerializer(serializers.ModelSerializer):
+    milestones = GoalMilestoneSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Goal
+        fields = '__all__'
+
+
+class RequisitionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Requisition
+        fields = '__all__'
+
+
+class TransactionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Transaction
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    transaction = TransactionSerializer(read_only=True)
+
+    class Meta:
+        model = Payment
+        fields = '__all__'
