0d1255366bb407b0d085b8c4cb4b983db38bc6a4
enoskyeza <enoskyeza@gmail.com>
2025-08-03 23:03:56 +0300

    remove redundant code

--- debug_toolbar/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Minimal stub of django-debug-toolbar for tests."""
-
-__all__ = ["urls"]
-
-from . import urls  # noqa: F401
-
--- debug_toolbar/urls.py
+++
@@ -1,8 +0,0 @@
-"""Stub URL configuration for django-debug-toolbar."""
-
-from django.urls import path
-
-urlpatterns = [
-    # No debug toolbar panels are provided in this stub.
-]
-
