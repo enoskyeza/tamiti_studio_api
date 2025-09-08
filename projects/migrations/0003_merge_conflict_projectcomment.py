6d84e4fb03a7d243e34d615595ed343b5e9f32f8
enoskyeza <enoskyeza@gmail.com>
2025-09-09 01:18:04 +0300

    update migrations

---
+++ projects/migrations/0003_merge_conflict_projectcomment.py
@@ -0,0 +1,12 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ("projects", "0002_alter_projectcomment_options"),
+        ("projects", "0002_delete_projectcomment"),
+    ]
+
+    operations = []
+
